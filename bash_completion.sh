a357544e7311847aad83565f1e06dfa29cee83fe
Kevin Clark <kevinclark19a@outlook.com>
2022-02-02 21:03:36 -0800

    update bash completion for bookshelves

--- bash_completion.sh
+++ bash_completion.sh
@@ -3,12 +3,13 @@
 __webtoepub_completion() {
 
     if [ $COMP_CWORD == 1 ]; then
-        COMPREPLY=($(compgen -W "story fetch" -- ${COMP_WORDS[COMP_CWORD]}))
+        COMPREPLY=($(compgen -W "story shelf fetch" -- ${COMP_WORDS[COMP_CWORD]}))
         return
     fi
     
     case ${COMP_WORDS[1]} in
         story) __webtoepub_story_completion ;;
+        shelf) __webtoepub_shelf_completion ;;
         fetch) __webtoepub_fetch_completion ;;
         *) COMPREPLY=() ;;
     esac
@@ -22,20 +23,14 @@ __webtoepub_story_completion() {
     fi
 
     case ${COMP_WORDS[2]} in
-        show) __webtoepub_story_handle_completion ;;
         list) __webtoepub_story_list_completion ;;
-        add) __webtoepub_story_add_completion ;;
+        show) __webtoepub_story_handle_completion ;;
         remove) __webtoepub_story_handle_completion ;;
         *) COMPREPLY=() ;;
     esac
 }
 
 __webtoepub_story_handle_completion() {
-    if [ $COMP_CWORD -ne 3 ]; then
-        COMPREPLY=()
-        return
-    fi
-
     local story_handles
 
     story_handles=`webtoepub story list --name-only | xargs printf "%s "`
@@ -51,16 +46,57 @@ __webtoepub_story_list_completion() {
     COMPREPLY=($(compgen -W "--name-only" -- ${COMP_WORDS[3]}))
 }
 
-__webtoepub_story_add_completion() {
-    # No real meaningful way to complete anyways, turn it off
-    COMPREPLY=()
+__webtoepub_shelf_completion() {
+    if [ $COMP_CWORD == 2 ]; then
+        COMPREPLY=($(compgen -W "show list create delete add remove" -- ${COMP_WORDS[2]}))
+        return
+    fi
+
+    case ${COMP_WORDS[2]} in
+        show) __webtoepub_shelf_handle_completion ;;
+        delete) __webtoepub_shelf_handle_completion ;;
+        add) __webtoepub_shelf_change_completion ;;
+        remove) __webtoepub_shelf_change_completion ;;
+        *) COMPREPLY=() ;;
+    esac
+}
+
+__webtoepub_shelf_handle_completion() {
+    local shelf_handles
+
+    shelf_handles=`webtoepub shelf list | xargs printf "%s "`
+    COMPREPLY=($(compgen -W "$shelf_handles" -- ${COMP_WORDS[3]}))
+}
+
+__webtoepub_shelf_change_completion() {
+    if [ $COMP_CWORD == 3 ]; then
+        __webtoepub_shelf_handle_completion
+        return
+    fi
+
+    local story_handles
+
+    story_handles=`webtoepub story list --name-only | xargs printf "%s "`
+    COMPREPLY=($(compgen -W "$story_handles" -- ${COMP_WORDS[COMP_CWORD]}))
 }
 
 __webtoepub_fetch_completion() {
     if [ $COMP_CWORD == 2 ]; then
-        COMPREPLY=($(compgen -W "one all" -- ${COMP_WORDS[2]}))
+        COMPREPLY=($(compgen -W "one all bookshelf" -- ${COMP_WORDS[2]}))
         return
     fi
 
-    COMPREPLY=()
+    if [ $COMP_CWORD == 3 ]; then
+        case ${COMP_WORDS[2]} in
+            one) __webtoepub_story_handle_completion ;;
+            bookshelf) __webtoepub_shelf_handle_completion ;;
+            *) COMPREPLY=() ;;
+        esac
+        return
+    fi
+
+    local story_handles
+
+    story_handles=`webtoepub story list --name-only | xargs printf "%s "`
+    COMPREPLY=($(compgen -W "$story_handles" -- ${COMP_WORDS[3]}))
 }
