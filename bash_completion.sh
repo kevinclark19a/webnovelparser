46ff080c867d9032d203d12d4af5fdb50d1e944b
Kevin Clark <kevinclark19a@outlook.com>
2022-07-12 12:07:59 -0700

    Updated non-code webtoepub -> webnovelparser

--- bash_completion.sh
+++ bash_completion.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-__webtoepub_completion() {
+__webnovelparser_completion() {
 
     if [ $COMP_CWORD == 1 ]; then
         COMPREPLY=($(compgen -W "story shelf fetch" -- ${COMP_WORDS[COMP_CWORD]}))
@@ -8,36 +8,36 @@ __webtoepub_completion() {
     fi
     
     case ${COMP_WORDS[1]} in
-        story) __webtoepub_story_completion ;;
-        shelf) __webtoepub_shelf_completion ;;
-        fetch) __webtoepub_fetch_completion ;;
+        story) __webnovelparser_story_completion ;;
+        shelf) __webnovelparser_shelf_completion ;;
+        fetch) __webnovelparser_fetch_completion ;;
         *) COMPREPLY=() ;;
     esac
 
 }
 
-__webtoepub_story_completion() {
+__webnovelparser_story_completion() {
     if [ $COMP_CWORD == 2 ]; then
         COMPREPLY=($(compgen -W "show list add remove" -- ${COMP_WORDS[2]}))
         return
     fi
 
     case ${COMP_WORDS[2]} in
-        list) __webtoepub_story_list_completion ;;
-        show) __webtoepub_story_handle_completion ;;
-        remove) __webtoepub_story_handle_completion ;;
+        list) __webnovelparser_story_list_completion ;;
+        show) __webnovelparser_story_handle_completion ;;
+        remove) __webnovelparser_story_handle_completion ;;
         *) COMPREPLY=() ;;
     esac
 }
 
-__webtoepub_story_handle_completion() {
+__webnovelparser_story_handle_completion() {
     local story_handles
 
-    story_handles=`webtoepub story list --name-only | xargs printf "%s "`
+    story_handles=`webnovelparser story list --name-only | xargs printf "%s "`
     COMPREPLY=($(compgen -W "$story_handles" -- ${COMP_WORDS[3]}))
 }
 
-__webtoepub_story_list_completion() {
+__webnovelparser_story_list_completion() {
     if [ $COMP_CWORD -ne 3 ]; then
         COMPREPLY=()
         return
@@ -46,41 +46,41 @@ __webtoepub_story_list_completion() {
     COMPREPLY=($(compgen -W "--name-only" -- ${COMP_WORDS[3]}))
 }
 
-__webtoepub_shelf_completion() {
+__webnovelparser_shelf_completion() {
     if [ $COMP_CWORD == 2 ]; then
         COMPREPLY=($(compgen -W "show list create delete add remove" -- ${COMP_WORDS[2]}))
         return
     fi
 
     case ${COMP_WORDS[2]} in
-        show) __webtoepub_shelf_handle_completion ;;
-        delete) __webtoepub_shelf_handle_completion ;;
-        add) __webtoepub_shelf_change_completion ;;
-        remove) __webtoepub_shelf_change_completion ;;
+        show) __webnovelparser_shelf_handle_completion ;;
+        delete) __webnovelparser_shelf_handle_completion ;;
+        add) __webnovelparser_shelf_change_completion ;;
+        remove) __webnovelparser_shelf_change_completion ;;
         *) COMPREPLY=() ;;
     esac
 }
 
-__webtoepub_shelf_handle_completion() {
+__webnovelparser_shelf_handle_completion() {
     local shelf_handles
 
-    shelf_handles=`webtoepub shelf list | xargs printf "%s "`
+    shelf_handles=`webnovelparser shelf list | xargs printf "%s "`
     COMPREPLY=($(compgen -W "$shelf_handles" -- ${COMP_WORDS[3]}))
 }
 
-__webtoepub_shelf_change_completion() {
+__webnovelparser_shelf_change_completion() {
     if [ $COMP_CWORD == 3 ]; then
-        __webtoepub_shelf_handle_completion
+        __webnovelparser_shelf_handle_completion
         return
     fi
 
     local story_handles
 
-    story_handles=`webtoepub story list --name-only | xargs printf "%s "`
+    story_handles=`webnovelparser story list --name-only | xargs printf "%s "`
     COMPREPLY=($(compgen -W "$story_handles" -- ${COMP_WORDS[COMP_CWORD]}))
 }
 
-__webtoepub_fetch_completion() {
+__webnovelparser_fetch_completion() {
     if [ $COMP_CWORD == 2 ]; then
         COMPREPLY=($(compgen -W "one all bookshelf" -- ${COMP_WORDS[2]}))
         return
@@ -88,8 +88,8 @@ __webtoepub_fetch_completion() {
 
     if [ $COMP_CWORD == 3 ]; then
         case ${COMP_WORDS[2]} in
-            one) __webtoepub_story_handle_completion ;;
-            bookshelf) __webtoepub_shelf_handle_completion ;;
+            one) __webnovelparser_story_handle_completion ;;
+            bookshelf) __webnovelparser_shelf_handle_completion ;;
             *) COMPREPLY=() ;;
         esac
         return
@@ -97,6 +97,6 @@ __webtoepub_fetch_completion() {
 
     local story_handles
 
-    story_handles=`webtoepub story list --name-only | xargs printf "%s "`
+    story_handles=`webnovelparser story list --name-only | xargs printf "%s "`
     COMPREPLY=($(compgen -W "$story_handles" -- ${COMP_WORDS[3]}))
 }
